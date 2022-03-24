b6138a0dbe918d8722b6c4541dd54755454a9ef0
Nicholas Schoeffler <nwschoeffler@gmail.com>
2022-03-24 08:40:00 -0500

    scratch page is working

--- Test_AP_project_scratch_page.py
+++ Test_AP_project_scratch_page.py
@@ -1,55 +1,114 @@
-import tensorflow as tf
-import keras
+#libararies
 import numpy as np
-import matplotlib
-matplotlib.use('TkAgg')
 import matplotlib.pyplot as plt
-import yfinance as yf 
+import pandas as pd
+import pandas_datareader as web
+import datetime as dt
+from sklearn.preprocessing import MinMaxScaler
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Dropout, LSTM
 
-# print("TensorFlow version:", tf.__version__)
+#loading company ticker
+companies = ['FB','GOOGL','AMZN','AAPL','TSLA'] #company list
+#prints the companies with a numerical value to allow the user easier selection
+for i in range(len(companies)):
+    print(f"{i + 1}. {companies[i]}")
+    i += 1
 
-# mnist = tf.keras.datasets.mnist
+#user selection
+company = input("Company would you like to use? Please type a number")
+chosen_company = companies[int(company)-1]# -1 resets the value to be the lists value
 
-# (x_train, y_train), (x_test, y_test) = mnist.load_data()
-# x_train, x_test = x_train / 255.0, x_test / 255.0
-# model = tf.keras.models.Sequential([
-#   tf.keras.layers.Flatten(input_shape=(28, 28)),
-#   tf.keras.layers.Dense(128, activation='relu'),
-#   tf.keras.layers.Dropout(0.2),
-#   tf.keras.layers.Dense(10)
-# ])
-# predictions = model(x_train[:1]).numpy()
-# predictions
-# tf.nn.softmax(predictions).numpy()
-# loss_fn = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
-# loss_fn(y_train[:1], predictions).numpy()
-# model.compile(optimizer='adam',
-#               loss=loss_fn,
-#               metrics=['accuracy'])
-# model.fit(x_train, y_train, epochs=5)
-# model.evaluate(x_test,  y_test, verbose=2)
-# probability_model = tf.keras.Sequential([
-#   model,
-#   tf.keras.layers.Softmax()
-# ])
-# probability_model(x_test[:5])
+#establish the start and end of the data
+start = dt.datetime(2012,1,1)
+end = dt.datetime.now()
 
-# data = yf.download('AAPL','2016-01-01','2022-01-01') 
-# X_list = []
-# X_list=X_list.append(data)
-# model = keras.Sequential([keras.layers.Dense(units=1,input_shape =[1])])
-# model.compile(optimizer ='sgd', loss = 'mean_squared_error')
+#uses yahoo finace
+data = web.DataReader(chosen_company, 'yahoo', start, end)
 
-xs = np.array([], dtype = float)
-ys = np.array([], dtype=float)
+#prepare Data fits them between 1 and 0 for simpliar prediction
+scaler = MinMaxScaler(feature_range = (0,1))
+scaled_data = scaler.fit_transform(data['Close'].values.reshape(-1,1))
 
-model = keras.Sequential([keras.layers.Dense(units=1,input_shape =[1])])
-model.compile(optimizer ='sgd', loss = 'mean_squared_error')
+#set the time period
+prediction_days = 60
 
-xs = np.array([-1.0,0.0,1.0,2.0,3.0,4.0], dtype = float)
-ys = np.array([-3.0,-1.0,1.0,3.0,5.0,7.0], dtype=float)
-model.fit(xs,ys,epochs = 500)
-print(model.predict([10.0]))
+#creates empty lists for the x and y training data
+x_train = []
+y_train = []
 
-#[[18.985796= 500]]
-#[[18.999987= 1000000]]
+#uses previous scaling to scale the data
+for i in range(prediction_days, len(scaled_data)):
+    # adds values to the training lists between the days
+    x_train.append(scaled_data[i - prediction_days:i,0])
+    y_train.append(scaled_data[i,0])
+
+x_train, y_train = np.array(x_train), np.array(y_train)
+x_train = np.reshape(x_train,(x_train.shape[0], x_train.shape[1],1))
+
+#building the model
+model = Sequential()
+
+#add one LSTM layer then one dropout and repeat
+#units = layers 
+model.add(LSTM(units = 50, return_sequences = True, input_shape= (x_train.shape[1],1)))
+model.add(Dropout(0.2))
+model.add(LSTM(units = 50, return_sequences = True))
+model.add(Dropout(0.2))
+model.add(LSTM(units = 50))
+model.add(Dropout(0.2))
+
+model.add(Dense(units= 1))#prediction of the next price
+
+#using adam and mean_squared_error
+model.compile(optimizer = 'adam', loss ='mean_squared_error')
+
+#epochs = how many times -1 the data will be seen
+model.fit(x_train,y_train,epochs = 20, batch_size = 32)
+
+#testing the model accuracy with existing data
+test_start = dt.datetime(2021,1,1)
+test_end = dt.datetime.now()
+
+
+test_data = web.DataReader(chosen_company, 'yahoo', test_start, test_end)
+actual_prices = test_data['Close'].values
+
+total_dataset = pd.concat((data['Close'],test_data['Close']), axis =0)
+
+model_inputs = total_dataset[len(total_dataset)-len(test_data)-prediction_days:].values
+model_inputs = model_inputs.reshape(-1,1)
+model_inputs = scaler.transform(model_inputs)
+
+#make predictions on test data
+
+x_test = []
+for i in range(prediction_days, len(model_inputs)):
+    x_test.append(model_inputs[i - prediction_days:i,0])
+
+x_test = np.array(x_test)
+x_test = np.reshape(x_test, (x_test.shape[0], x_test.shape[1], 1))
+
+predicted_prices =model.predict(x_test)
+predicted_prices = scaler.inverse_transform(predicted_prices)
+
+#plotting the test predictions
+plt.plot(actual_prices, color = "red", label = f"Actual {chosen_company} Price")
+plt.plot(predicted_prices, color = "blue", label = f"Predicted {chosen_company} Price")
+plt.title(f"{chosen_company} Share Price")
+plt.xlabel('Time')
+plt.ylabel(f'{chosen_company} Share Price')
+plt.legend()
+
+
+# predicts next days price
+real_data = [model_inputs[len(model_inputs) + 1 - prediction_days:len(model_inputs + 1), 0]]
+real_data = np.array(real_data)
+real_data= np.reshape(real_data,(real_data.shape[0], real_data.shape[1],1))
+
+prediction = model.predict(real_data)
+prediction = scaler.inverse_transform(prediction)
+
+print(f"Prediction: {prediction}")
+
+plt.show()
